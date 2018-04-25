51233969a71c59308bbbb22ac0ae49866a9394ec
jeilones <jeisolon.espi@gmail.com>
2018-04-25 18:19:57 +0300

    Unzip CSV files instead of Geojson files and upload its properties to the database.

--- codes/src/travelTimeMatrixOperations/SpatialPatterns.py
+++ codes/src/travelTimeMatrixOperations/SpatialPatterns.py
@@ -11,29 +11,35 @@ class SpatialPatterns(object):
 
     @dgl_timer
     def insertData(self, travelTimeMatrixURL, tableName, columns, outputFolder):
-        travelTimeMatrix = self.comparison.calculateTravelTime(travelTimeMatrixURL)
-
-        travelTimeMatrix = self.postGISServiceProvider.renameColumnsAndExtractSubSet(
-            travelTimeMatrix=travelTimeMatrix,
-            columns=columns
-        )
-        # travelTimeMatrix = self.postGISServiceProvider.insertableTravelTimeMatrixGeoDataFrame(
-        #     travelTimeMatrix=travelTimeMatrix, tableName=tableName, column1="ykr_from_id",
-        #     column2="ykr_to_id")
-
-        # isExecuted = self.postGISServiceProvider.insert(dataFrame=travelTimeMatrix,
-        #                                                 tableName=tableName)
-
-        csv_filename = tableName + ".csv"
-        FileActions().createFile(outputFolder, csv_filename)
-
         csv_separator = ';'
 
-        csv_path = os.path.join(outputFolder, csv_filename)
-        travelTimeMatrix.to_csv(csv_path, sep=csv_separator, index=False)
-        columns = tuple(travelTimeMatrix.columns.values)
+        # travelTimeMatrix = self.comparison.calculateTravelTime(travelTimeMatrixURL)
+        #
+        # travelTimeMatrix = self.postGISServiceProvider.renameColumnsAndExtractSubSet(
+        #     travelTimeMatrix=travelTimeMatrix,
+        #     columns=columns
+        # )
+        # # travelTimeMatrix = self.postGISServiceProvider.insertableTravelTimeMatrixGeoDataFrame(
+        # #     travelTimeMatrix=travelTimeMatrix, tableName=tableName, column1="ykr_from_id",
+        # #     column2="ykr_to_id")
+        #
+        # # isExecuted = self.postGISServiceProvider.insert(dataFrame=travelTimeMatrix,
+        # #                                                 tableName=tableName)
+        #
+        # csv_filename = tableName + ".csv"
+        # FileActions().createFile(outputFolder, csv_filename)
+        #
+        #
+        #
+        # csv_path = os.path.join(outputFolder, csv_filename)
+        # travelTimeMatrix.to_csv(csv_path, sep=csv_separator, index=False)
+        # columns = tuple(travelTimeMatrix.columns.values)
+        #
+        # csv_file = open(csv_path, 'r')
+        # csv_file.readline()  # dismiss the first row with the column names
 
-        csv_file = open(csv_path, 'r')
+
+        csv_file = open(travelTimeMatrixURL, 'r')
         csv_file.readline()  # dismiss the first row with the column names
 
         isExecuted = self.postGISServiceProvider.copyData(csvData=csv_file,
