61fdea9d007738037f4ea21828b257629428198d
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2019-12-04 18:41:25 +0000

    Fix paths

--- codes/tests/comparisons-against-Google-and-OTP/run_otp_validation_tests_walk.py
+++ codes/tests/comparisons-against-Google-and-OTP/run_otp_validation_tests_walk.py
@@ -8,18 +8,25 @@ Created on Thu Jun 13 11:16:36 2019
 
 @author: hentenka
 """
+
 import os
+from inspect import getsourcefile
+
+def get_current_dir():
+    """Get directory of current file (i.e. where utils.py is located)"""
+    return os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
 
 # Filepaths
-mrouter_path = r"C:\HY-DATA\hentenka\KOODIT\Mapple\mrouter\mrouter\one_to_many_otp.py"
+mrouter_path = os.path.join(get_current_dir(), "one_to_many_otp.py")
 otp_path = "otp-1.3.0-shaded.jar"
-base_dir = r"C:\HY-DATA\hentenka\KOODIT\Uni\manuscripts\2019_TravelTimeMatrix\data"
+base_dir = "data"
 data_dir = os.path.join(base_dir, 'otp-data', 'Helsinki')
-output_dir = os.path.join(base_dir, 'validation', 'results')
+output_dir = os.path.join(base_dir, 'validation')
 conf_fp = os.path.join(data_dir, 'config_walk.json')
-origs_fp = os.path.join(base_dir, 'validation', 'validation_sample_100_points.csv')
-#origs_fp = os.path.join(base_dir, 'validation', 'validation_sample_test.csv')
+origs_fp = os.path.join(base_dir, 'validation_sample_100_points.csv')
 dests_fp = origs_fp
+
+# Params
 name = "OTP_results_for_validation_Jan_29_2018_walk"
 orig_id = 'ID'
 dest_id = 'ID'
@@ -38,19 +45,5 @@ cmd = "java -cp {otp};jython27.jar org.python.util.jython {router} -c {conf} -g 
         orig_id = orig_id,
         dest_id = dest_id
               )
-
-# Construct detailed routings
-#cmd = "python {router} -c {conf} -g {graph} -o {origins} -d {destinations} -O {outdir} -n {name} -i {orig_id} -I {dest_id}".format(
-#        otp=otp_path, 
-#        router=mrouter_path,
-#        conf=conf_fp,
-#        graph=data_dir,
-#        origins=origs_fp,
-#        destinations=dests_fp,
-#        outdir = output_dir,
-#        name = name,
-#        orig_id = orig_id,
-#        dest_id = dest_id
-#              )
 
 print("Run from command prompt:\n", cmd)
